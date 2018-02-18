fbdb224079378cd39bc700824540e8d4c0806d7a
Evan Martin <evan.martin@shaw.ca>
2018-02-18 14:28:23 -0800

    making things prettier

--- overlayer.py
+++ overlayer.py
@@ -2,16 +2,17 @@ from PIL import Image, ImageDraw, ImageFont, ImageChops
 import textwrap
 import os
 
+# gives the background image a grayscale to allow easier reading of quote
 def apply_tint(im, tint_color):
 	tinted_im = ImageChops.multiply(im, Image.new('RGB', im.size, tint_color))
 	return tinted_im
 
-
+# places the trademark logo at the bottom of the image (hardcoded placement)
 def place_logo(bkg, logo):
 	bkg.paste(logo, ((540-65/2), (1010-65)), logo)
 	return bkg
 
-
+# places the trademark logo at the bottom of the image (hardcoded placement)
 def place_trademark(im, trademark, font):
 	draw = ImageDraw.Draw(im)
 	bbox =  im.getbbox()
@@ -20,32 +21,33 @@ def place_trademark(im, trademark, font):
 	draw.text(((W-800/2)/2, 1010), trademark, font=font)
 	return im
 
-
-def place_caption(im, cap, font):
+# places the quote in the centre of the image
+def place_quote(im, quote, font):
 	draw = ImageDraw.Draw(im)
-	w, h = draw.textsize(cap, font=font)
+	w, h = draw.textsize(quote, font=font)
 	bbox =  im.getbbox()
-	W = bbox[2]
-	H = bbox[3]
+	W = bbox[2]; H = bbox[3]
 
-	lines = textwrap.wrap(cap, width=24)
+	# determine the number of lines needed to fit the quote on the image
+	lines = textwrap.wrap(quote, width=24)
 	n_lines = len(lines)
 	pad = -10
  
+ 	# place the lines of the quotes one on top of the other
 	current_h = H/2 - (n_lines*h/2)
 	for line in lines:
 		w, h = draw.textsize(line, font=font)
 		draw.text(((W - w) / 2, current_h), line, font=font)
 		current_h += h + pad
 
-
+# determine is the given path is an image
 def is_img(path):
 	ext = path[-4:]
 	if (ext == ".jpg") or (ext == ".png") or (ext == "jpeg"):
 		return True
 	return False
 
-
+# gets the paths of the images to be used
 def get_im_paths(files):
 	pic_paths = []
 	for file in files:
@@ -53,22 +55,23 @@ def get_im_paths(files):
 			pic_paths.append(file)
 	return pic_paths
 
-
-def get_captions(file):
+# reads the quotes from the quotes.txt file 
+def get_quotes(file):
 	with open(file) as f:
 		content = f.readlines()
 	return [x.strip() for x in content] 
 
-
-def caption_image(im_path, cap, im_count = '', logoify = True):
+# creates and saves an image
+def build_image(im_path, quote, im_count = '', logoify = True):
 	W = H = 1080
 	im = Image.open(im_path).resize((W,H))
 	im = apply_tint(im, (200,200,200))
 	draw = ImageDraw.Draw(im)
 
 	cap_font = ImageFont.truetype("utils/BebasNeue.otf",115)
-	place_caption(im, cap, cap_font)
+	place_quote(im, quote, cap_font)
 
+	# if the 'add trademark' option is selected then add the logo and trademark
 	if (logoify):
 		trademark = "Lifting.Motivations"
 		tm_font = ImageFont.truetype("utils/BebasNeue.otf",62)
@@ -76,14 +79,14 @@ def caption_image(im_path, cap, im_count = '', logoify = True):
 		logo = Image.open("utils/logo.png").resize((65,65))
 		place_logo(im, logo)
 
-	im.save('out/' + str(im_count) + "_" + str(cap[0:10]) + '.png')
-	print "Output image saved as: " + 'out/' + str(im_count) + "_" + str(cap[0:10]) + '.png'
+	im.save('out/' + str(im_count) + "_" + str(quote[0:10]) + '.png')
+	print "Output image saved as: " + 'out/' + str(im_count) + "_" + str(quote[0:10]) + '.png'
 
 
 def main():
-	dir_paths = os.listdir("../captioners/in/bkg")
+	dir_paths = os.listdir("../overlayer/in/raw")
 	im_paths = get_im_paths(dir_paths)
-	caps = get_captions("../captioners/in/cap.txt")
+	quotes = get_quotes("../overlayer/in/quotes.txt")
 
 	permute = (raw_input("Generate all permutations? (y/n): ") == 'y')
 	logoify = (raw_input("Include trademark/logo? (y/n): ") == 'y')
@@ -91,13 +94,13 @@ def main():
 	if permute:
 		im_count = 0
 		for im_path in im_paths:
-			for cap in caps:
-				print "Captioning " + str(im_path) + "..."
-				caption_image('in/bkg/' + im_path, cap, im_count, logoify)
+			for quote in quotes:
+				print "Overlaying " + str(im_path) + "..."
+				build_image('in/raw/' + im_path, quote, im_count, logoify)
 			im_count = im_count + 1
 	else:	
 		for i, im_path in enumerate(im_paths):
-			print "Captioning " + str(im_path) + "..."
-			caption_image('in/bkg/' + im_path, caps[i], '', logoify)
+			print "Overlaying " + str(im_path) + "..."
+			build_image('in/raw/' + im_path, quotes[i], '', logoify)
 
 main()
